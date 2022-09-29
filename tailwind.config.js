308d94ae9ccbd2246f60d082acaeec2ca5ed8866
Abdulrahman Alblooshi <aboodshadow@outlook.com>
2022-09-30 00:10:44 +0400

    New Font and Text Centering

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
@@ -8,7 +10,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        Archivo: "Archivo Regular",
+        sans: ['Archivo', ...defaultTheme.fontFamily.sans]
       },
     },
   },
